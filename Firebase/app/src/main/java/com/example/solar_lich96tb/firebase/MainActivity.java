35cd10d952f98b510ae66b62e7b749f21c83b77b
lichdt <lich96tb@gmail.com>
2018-11-28 01:04:02 +0700

    oK

--- Firebase/app/src/main/java/com/example/solar_lich96tb/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/example/solar_lich96tb/firebase/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.solar_lich96tb.firebase;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,11 +30,14 @@ public class MainActivity extends AppCompatActivity {
     List<User> list;
     private DatabaseReference mDatabase;
     EditText edtSen;
-    Button btnCheck;
+    LinearLayout btnCheck;
     EditText edtLink;
-    TextView txtCheckIsPlay;
+    private TextView txtCheckIsPlay, valueVolume;
     private boolean checkplay;
     private String url;
+    private ImageView imgIsPlay;
+    private LinearLayout layoutVolume,dialog_display,linearLayout;
+    private SeekBar seekbarVolume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +45,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         nghe();
+        volume();
 
 
+    }
+
+    private void volume() {
+        seekbarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    valueVolume.setText(progress + " %");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mDatabase.child("Volume").setValue(seekBar.getProgress());
+            }
+        });
     }
 
     private void nghe() {
@@ -48,11 +79,11 @@ public class MainActivity extends AppCompatActivity {
                 url = user.getLink();
                 checkplay = user.isCheckplay();
                 if (user.isCheckplay()) {
-                    btnCheck.setText("tạm dừng");
+                    imgIsPlay.setImageResource(R.drawable.ic_pause);
                     txtCheckIsPlay.setText("Bài hát đang chạy trên máy con");
                 } else {
-                    btnCheck.setText("tiếp tục");
                     txtCheckIsPlay.setText("Bài hát đang không chạy trên máy con");
+                    imgIsPlay.setImageResource(R.drawable.ic_play);
                 }
             }
 
@@ -65,6 +96,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initView() {
+        linearLayout=findViewById(R.id.linearLayout);
+        dialog_display=findViewById(R.id.dialog_display);
+        valueVolume = findViewById(R.id.valueVolume);
+        seekbarVolume = findViewById(R.id.seekbarVolume);
+        layoutVolume = findViewById(R.id.layoutVolume);
+        imgIsPlay = findViewById(R.id.imgIsPlay);
         txtCheckIsPlay = findViewById(R.id.txtCheckIsPlay);
         edtLink = findViewById(R.id.edtLinkNhac);
         btnCheck = findViewById(R.id.btnCheck);
@@ -83,5 +120,17 @@ public class MainActivity extends AppCompatActivity {
     public void PlaySongLink(View view) {
         url = edtLink.getText().toString().trim();
         mDatabase.child("chatroom").setValue(new User(true, url, true));
+    }
+
+    public void setVolume(View view) {
+        if (layoutVolume.getVisibility() == View.VISIBLE) {
+            layoutVolume.setVisibility(View.GONE);
+            dialog_display.setVisibility(View.GONE);
+            linearLayout.setBackgroundResource(R.color.colorNormal);
+        } else {
+            linearLayout.setBackgroundResource(R.color.colorActive);
+            layoutVolume.setVisibility(View.VISIBLE);
+            dialog_display.setVisibility(View.VISIBLE);
+        }
     }
 }
